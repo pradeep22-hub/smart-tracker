42b9389985859a884a9e6a83ce8008776f018165
alishbam76001 <alishbam76001@gmail.com>
2025-11-04 03:58:50 -0800

    redirect to dashboard

--- src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ src/app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,5 +1,14 @@
-import { SignIn } from '@clerk/nextjs'
+import { SignIn } from "@clerk/nextjs";
 
-export default function Page() {
-  return <SignIn />
+export default function SignInPage() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <SignIn
+        routing="path"
+        path="/sign-in"
+        signUpUrl="/sign-up"
+        
+      />
+    </div>
+  );
 }
